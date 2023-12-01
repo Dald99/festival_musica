23832721abceed03da0b47248efde108dd24516a
Dald99 <dani_lara99@hotmail.com>
2023-12-01 13:51:44 -0800

    Parte 1

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        './index.html',
+        "./src/**/*.{html,js}",
+    ],
+    theme: {
+        fontFamily: {
+            'mont': ['Montserrat', 'sans-serif'],
+        },
+        extend: {},
+    },
+    plugins: [],
+}
+
